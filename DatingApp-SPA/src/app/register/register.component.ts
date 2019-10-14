00206a70965c2b6e342e9478727e2b3ef5614f71
Prabhat Kumar <prabhatkumar@rarecrew.com>
2019-10-14 12:21:41 +0530

    3rd party lib-AlertifyJs,Ngx bootstarp,Angular Jwt

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -9,17 +10,20 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   register() {
     this.authService.register(this.model).subscribe(
       () => {
-        console.log('Register successful');
+        this.alertify.success('Registerationng  successful');
       },
       error => {
-        console.log(error);
+        this.alertify.error(error);
       }
     );
   }
